d92c547aa808af080ca45fb94a8d30441b77bc12
Akshay <akshaychoudhary2040@gmail.com>
2024-09-07 17:12:40 +0530

    Import_data.html Page Form Ready.

---
+++ awd_main/data_entry/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('import_data/',views.import_data,name='import_data')
+]
--- awd_main/data_entry/views.py
+++ awd_main/data_entry/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request,'data_entry/home.html')
+
+
+def import_data(request):
+    return render(request,'data_entry/import_data.html')
