e496d5fcb93ed0f9a99ae8054a31aa7ea80072d4
Akshay <akshaychoudhary2040@gmail.com>
2024-09-08 00:17:26 +0530

    Deploying....

--- awd_main/awd_main/settings.py
+++ awd_main/awd_main/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-wf8i1%akp!^@4ob11+yqc*jxkwh%8d()0fs3v2t#7k3bc51ujd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
